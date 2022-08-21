9af33d8eaced54b66d33e65f9e81c2ff81106c10
Mauricio Silva <mausv97@gmail.com>
2022-08-20 22:21:08 -0500

    Add audio to rocket.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     private Rigidbody rb;
+    private AudioSource audioSource;
     [SerializeField] float mainThrust = 100.0f;
     [SerializeField] float rotationSpeed = 100.0f;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -25,6 +27,9 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime); // Shorthand for 0, 1, 0
+            if (!audioSource.isPlaying) audioSource.Play();
+        } else {
+            audioSource.Stop();
         }
     }
 
