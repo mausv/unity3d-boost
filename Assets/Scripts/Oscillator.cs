87c897c88e4d9f9f7d23ce30215d6c19e4c90a89
Mauricio Silva <mausv97@gmail.com>
2022-08-21 16:17:12 -0500

    Add oscillation effect with Mathf.Sin and Tau.

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -6,7 +6,9 @@ public class Oscillator : MonoBehaviour
 {
     Vector3 startingPosition;
     [SerializeField] Vector3 movementVector;
-    [SerializeField] [Range(0,1)] float movementFactor;
+    // [SerializeField] [Range(0,1)] 
+    float movementFactor;
+    [SerializeField] float period = 2.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +19,14 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (period <= Mathf.Epsilon) return;
+
+        float cycles = Time.time / period; // Continually growing over time
+        const float tau = Mathf.PI * 2; // Constant value of 6.283
+        float rawSinWave = Mathf.Sin(cycles * tau); // Going from -1 to 1
+
+        movementFactor = (rawSinWave + 1.0f) / 2.0f; // Take our value from -1 - 1 to 0 - 1
+
         Vector3 offset = movementVector * movementFactor;
         Vector3 newPosition = startingPosition + offset;
         transform.position = newPosition;
